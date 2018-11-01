cdbd229971d4530447a0f497b9239c0da49e06c9
fabioblanco168 <fabioblanco168@gmail.com>
2018-11-01 13:40:02 -0500

    version 2 de la seccion formacion

--- js/main.js
+++ js/main.js
@@ -33,3 +33,14 @@ $(window).scroll(function() {
 				}
 		});
 }).scroll();
+
+var swiper = new Swiper('.swiper-container', {
+	pagination: {
+	  el: '.swiper-pagination',
+	  type: 'progressbar',
+	},
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  });
